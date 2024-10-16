25f1e3aaf6c242cbf251224ce2dce446ca551b75
SUBHANIQBAL-STUDENT <G00433839@atu.ie>
2024-10-16 16:24:08 +0100

    First commit the base code

---
+++ src/main/java/org/example/Arrays.java
@@ -0,0 +1,24 @@
+package org.example;
+
+public class Arrays {
+    public static void main(String[] args) {
+        int[] Array;
+
+        Array =  new int[10];
+
+        Array[0] = 100;
+        Array[1] = 200;
+        Array[2] = 300;
+        Array[3] = 400;
+        Array[4] = 500;
+        Array[5] = 600;
+        Array[6] = 700;
+        Array[7] = 800;
+        Array[8] = 900;
+        Array[9] = 1000;
+
+        System.out.println("Element at index 0: "+ Array[0]);
+
+
+    }
+}
